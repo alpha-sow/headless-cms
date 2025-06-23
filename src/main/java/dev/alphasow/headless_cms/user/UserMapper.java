56768f60ff126abcff71fff471c900d5161b9eab
alphasow <alphaibrahima.sow@outlook.fr>
2025-06-23 17:06:33 +0200

    refactor: Update `UserMapper` injection and usage across `UserService`

--- src/main/java/dev/alphasow/headless_cms/user/UserMapper.java
+++ src/main/java/dev/alphasow/headless_cms/user/UserMapper.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.PageImpl;
 import java.util.List;
 import java.util.Set;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     UserMapper instance = Mappers.getMapper(UserMapper.class);
 
