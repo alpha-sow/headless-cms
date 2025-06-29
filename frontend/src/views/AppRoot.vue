bc71816aa5a1892ae304fc700884ec83b87743dc
alphasow <alphaibrahima.sow@outlook.fr>
2025-06-29 16:04:01 +0200

    feat: Migrate frontend to `frontend`, add Docker setup and update configurations

--- frontend/src/views/AppRoot.vue
+++ frontend/src/views/AppRoot.vue
@@ -2,7 +2,7 @@
 import { authenticationUser } from "@/services";
 import { useQuery } from "@tanstack/vue-query";
 import { RouterView } from "vue-router";
-const { data } = useQuery({
+useQuery({
   queryKey: ["userAuth"],
   queryFn: authenticationUser,
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+          target: process.env.BACKEND_URL || "http://localhost:8080/api",
+          changeOrigin: true, 
+          rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
